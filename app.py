e67529ee9bcbd6b84372c638bd9ebe4a676328be
elixter <ltw971@naver.com>
2021-08-23 15:35:20 +0900

    feat: Fit handler prototype

--- app.py
+++ app.py
@@ -1,32 +1,64 @@
+import flask
 from flask import Flask
 from urllib import request as req
 import zipfile
 import tensorflow as tf
+import json
+import pandas as pd
+import train
+from sklearn.preprocessing import MinMaxScaler
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    r = req.Request("http://127.0.0.1:8080/getmodel")
-
-    open('model.py', 'wb').write(req.urlopen(r).read())
-
     return 'Hello World!'
 
 
-@app.route('/run')
+@app.route('/run', methods=['GET', 'POST'])
 def run():
     # Get Saved model and Unzip
-    r = req.Request("http://127.0.0.1:8080/getmodel")
+    r = req.Request("http://127.0.0.1:8080/model")
 
-    open('./model.zip', 'wb').write(req.urlopen(r).read())
+    open('./Model.zip', 'wb').write(req.urlopen(r).read())
 
-    with zipfile.ZipFile('./model.zip', 'r') as zip_ref:
+    with zipfile.ZipFile('./Model.zip', 'r') as zip_ref:
         zip_ref.extractall('./')
+        print('extracting...')
 
     # Load model
-    model = tf.keras.models.load_model('./MNIST')
+    model = tf.keras.models.load_model('./Model')
+
+    # Load dataset
+    body = flask.request.data
+    body = json.loads(body)
+
+    data_set = body['data_set']
+
+    df = pd.read_csv(data_set['train_uri'])
+    label = df[data_set['label']]
+    data = df.drop(axis=1, columns=[data_set['label']])
+
+    # Get shape for data set reshaping for compatible with input shape of model.
+    input_shape = list(*model.layers[0].output_shape)
+
+    for i, val in enumerate(input_shape):
+        if val == None:
+            input_shape[i] = -1
+
+    # Data Normalization
+    mms = MinMaxScaler()
+    data = mms.fit_transform(data)
+
+    # Data reshape.
+    data = data.reshape(input_shape)
+
+    # Fit model
+    train.fit_model(model, data, label)
+
+    return "I'm gonna fucking mad."
 
 
 if __name__ == '__main__':
---
+++ train.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+def fit_model(model, data, label, validation_split=0.3):
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=2)
+    learning_rate_reduction = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', patience=2, verbose=1,
+                                                                   factor=0.25, min_lr=3e-07)
+    remote_monitor = tf.keras.callbacks.RemoteMonitor(root='http://localohst:8080', path='/publish/epoch/end',
+                                                      field='data', headers=None, send_as_json=True)
+
+    model.fit(
+        data, label,
+        epochs=10,
+        batch_size=32,
+        validation_split=validation_split,
+        callbacks=[remote_monitor, learning_rate_reduction, early_stop]
+    )
+
